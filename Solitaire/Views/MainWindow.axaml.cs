2dfe565b8e1afa597efdee2da7d239fd54454784
Dan Walmsley <dan@walms.co.uk>
2022-10-24 18:51:16 +0100

    devtools

--- Solitaire/Views/MainWindow.axaml.cs
+++ Solitaire/Views/MainWindow.axaml.cs
@@ -1,3 +1,4 @@
+using Avalonia;
 using Avalonia.Controls;
 
 namespace Solitaire.Views;
@@ -7,5 +8,9 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+#if DEBUG
+        this.AttachDevTools();  
+#endif
     }
 }
