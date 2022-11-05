cbe1346190a5b301c84e237b9c8f2c67461c910e
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2022-11-05 22:42:20 +0800

    fix reflection usage

---
+++ Solitaire/Behaviors/CompositionBehaviors.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Reactive.Linq;
+using Avalonia;
+using Avalonia.Animation.Easings;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Rendering.Composition.Animations;
+using Avalonia.Xaml.Interactions.Custom;
+
+namespace Solitaire.Behaviors;
+
+public abstract class CompositionBehaviors<T> : DisposingBehavior<T> where T : class, IAvaloniaObject
+{
+    public TimeSpan Duration { get; set; }
+    public TimeSpan Delay { get; set; }
+    public IEasing? Easing { get; set; }
+}
