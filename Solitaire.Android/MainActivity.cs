d6f288198aaa7e272da64dfdbf5e70b0f5368299
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-14 22:40:17 +0100

    Fix android

--- Solitaire.Android/MainActivity.cs
+++ Solitaire.Android/MainActivity.cs
@@ -1,14 +1,11 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Avalonia;
+using Avalonia.Android;
 
 namespace Solitaire.Android;
 
 [Activity(Label = "Solitaire.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleInstance, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
-public class MainActivity : AvaloniaActivity<App>
+public class MainActivity : AvaloniaMainActivity
 {
-    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
-    {
-        return base.CustomizeAppBuilder(builder);
-    }
 }
