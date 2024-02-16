e31577de6eb62e019a46f4d9b4ecc43845cec873
Max Katz <maxkatz6@outlook.com>
2024-02-15 17:30:06 -0800

    Handle safe areas

--- Solitaire/Controls/DeckControls.axaml.cs
+++ Solitaire/Controls/DeckControls.axaml.cs
@@ -1,4 +1,6 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Threading;
 
 namespace Solitaire.Controls;
 
@@ -7,5 +9,26 @@ public partial class DeckControls : UserControl
     public DeckControls()
     {
         InitializeComponent();
+    }
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+
+        if (change.Property == BoundsProperty)
+        {
+            // When there is no "safe area" offset, we want to partially hide TrayContainer.
+            // Dispatcher helps with async resizing on some platforms.
+            Dispatcher.UIThread.Post(() =>
+            {
+                var hasOffset = TopLevel.GetTopLevel(this) is { } topLevel
+                                && this.TransformToVisual(topLevel) is { } transform
+                                && new Rect(default, Bounds.Size).TransformToAABB(transform) is var transformedRect
+                                && topLevel.Bounds.Height - transformedRect.Bottom > 1;
+
+                TrayContainer.Classes.Set("downDeck", !hasOffset);
+                TrayContainer.Margin = hasOffset ? default : new Thickness(0, 0, 0, -15);
+            });
+        }
     }
 }
--- Solitaire/Views/CasinoView.axaml.cs
+++ Solitaire/Views/CasinoView.axaml.cs
@@ -1,4 +1,11 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Platform;
+using Avalonia.VisualTree;
+using Solitaire.Controls;
+
+#pragma warning disable CS0618 // Type or member is obsolete
 
 namespace Solitaire.Views;
 
@@ -13,5 +20,40 @@ public partial class CasinoView : UserControl
     public CasinoView()
     {
         InitializeComponent();
-    }   
+    }
+
+    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnAttachedToVisualTree(e);
+
+        if (TopLevel.GetTopLevel(this) is { InsetsManager: { } insetsManager })
+        {
+            insetsManager.SafeAreaChanged += InsetsManagerOnSafeAreaChanged;
+            InsetsManagerOnSafeAreaChanged(insetsManager, new SafeAreaChangedArgs(insetsManager.SafeAreaPadding));
+        }
+        else
+        {
+            InsetsManagerOnSafeAreaChanged(this, new SafeAreaChangedArgs(default));
+        }
+    }
+
+    protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnDetachedFromVisualTree(e);
+
+        if (e.Root is TopLevel { InsetsManager: { } insetsManager })
+        {
+            insetsManager.SafeAreaChanged -= InsetsManagerOnSafeAreaChanged;
+        }
+    }
+
+    private void InsetsManagerOnSafeAreaChanged(object? sender, SafeAreaChangedArgs e)
+    {
+        // Apply "10,10,10,0" as a minimum padding.
+        RootContentControl.Padding = new Thickness(
+            Math.Max(10, e.SafeAreaPadding.Left),
+            Math.Max(10, e.SafeAreaPadding.Top),
+            Math.Max(10, e.SafeAreaPadding.Right),
+            e.SafeAreaPadding.Bottom);
+    }
 }
