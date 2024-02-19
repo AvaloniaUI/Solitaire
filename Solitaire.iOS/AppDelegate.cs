6af07f7f34dba6cb21ea3f59906e9479c5d4c5b7
Max Katz <maxkatz6@outlook.com>
2024-02-18 21:59:06 -0800

    Work on iOS icons

--- Solitaire.iOS/AppDelegate.cs
+++ Solitaire.iOS/AppDelegate.cs
@@ -8,9 +8,7 @@ namespace Solitaire.iOS;
 // User Interface of the application, as well as listening (and optionally responding) to 
 // application events from iOS.
 [Register("AppDelegate")]
-#pragma warning disable CA1711 // Identifiers should not have incorrect suffix
-public partial class AppDelegate : AvaloniaAppDelegate<App>
-#pragma warning restore CA1711 // Identifiers should not have incorrect suffix
+public class AppDelegate : AvaloniaAppDelegate<App>
 {
     protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
     {
